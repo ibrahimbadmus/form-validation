2082fdf2077fc7032a04b7f9f0332cebb62f62d1
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-12-09 21:22:45 +0100

    updated form validation

--- form.js
+++ form.js
@@ -1,133 +1,79 @@
-const form = document.getElementById("form");
-const firstName = document.getElementById("firstName");
-const LastName = document.getElementById("lastName");
-const email = document.getElementById("email");
-const newPassword = document.getElementById("newPassword");
-const confirmPassword = document.getElementById("confirmPassword");
-const userName =document.getElementById("username");
+const form  = getElementById ('form');
+const firstName = getElementById ('firstName');
+const lastName = getElementById ('lastName');
+const email = getElementById ('email');
+const userName = getElementById ('userName');
+const newPassword = getElementById ('newPassword');
+const confirmPassword = getElementById ('confirmPassword');
 
-const checkUsername = () => {
-
-    let valid = false;
-
-    const min = 5,
-        max = 25;
-
-    const username = userName.value.trim();
-
-    if (!isRequired(userName)) {
-        showError(userName, 'Username cannot be blank.');
-    } else if (!isBetween(userName.length, min, max)) {
-        showError(userName, `Username must be between ${min} and ${max} characters.`)
-    } else {
-        showSuccess(userName);
-        valid = true;
-    }
-    return valid;
-};
-    const checkEmail = () => {
-    let valid = false;
-    const email1 = email.value.trim();
-    if (!isRequired(email1)) {
-        showError(email1, 'Email cannot be blank.');
-    } else if (!isEmailValid(email)) {
-        showError(email, 'Email is not valid.')
-    } else {
-        showSuccess(email);
-        valid = true;
-    }
-    return valid;
-};
-const checkPassword = () => {
-    let valid = false;
-
-
-    const password = newPassword.value.trim();
-
-    if (!isRequired(password)) {
-        showError(newPassword, 'Password cannot be blank.');
-    } else if (!isPasswordSecure(password)) {
-        showError(newPassword, 'Password must have at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
-    } else {
-        showSuccess(newPassword);
-        valid = true;
-    }
-
-    return valid;
-};
-
-    const checkConfirmPassword = () => {
-    let valid = false;
-    // check confirm password
-    const confirmPassword = confirmPassword.value.trim();
-    const password = newPassword.value.trim();
-
-    if (!isRequired(confirmPassword)) {
-        showError(confirmPassword, 'Please enter the password again');
-    } else if (password !== confirmPassword) {
-        showError(confirmPassword, 'The password does not match');
-    } else {
-        showSuccess(confirmPassword);
-        valid = true;
-    }
-
-    return valid;
-};
-    const isEmailValid = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-};
-const isPasswordSecure = (password) => {
-    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-    return re.test(password);
-};
-
-const isRequired = value => value === '' ? false : true;
-const isBetween = (length, min, max) => length < min || length > max ? false : true;
-
-const showError = (input, message) => {
-    // get the form-field element
-    const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('success');
-    formField.classList.add('error');
-
-    // show the error message
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
-
-const showSuccess = (input) => {
-    // get the form-field element
-    const formField = input.parentElement;
-
-    // remove the error class
-    formField.classList.remove('error');
-    formField.classList.add('success');
-
-    // hide the error message
-    const error = formField.querySelector('small');
-    error.textContent = '';
-}
-
-
-form.addEventListener ("submit", function (e) {
-    //prevent the form from submitting
+form.addEventListener('submit', e => {
     e.preventDefault();
-// validate fields
-let isUsernameValid = checkUsername(),
-isEmailValid = checkEmail(),
-isPasswordValid = checkPassword(),
-isConfirmPasswordValid = checkConfirmPassword();
+    
+    validateInputs();
+    });
 
-let isFormValid = isUsernameValid &&
-isEmailValid &&
-isPasswordValid &&
-isConfirmPasswordValid;
+    const setError = (element, message) => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+        
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success')
+    }
 
-// submit to the server if the form is valid
-if (isFormValid) {
+    const setSuccess = element => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
 
-}
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error');
+    };
 
-});
+    const isValidEmail = email => {
+        const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        return re.test(string(email).toLowerCase());
+    }
+
+const validateInputs = () => {
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const emailValue = email.value.trim();
+    const userNameValue = userName.value.trim();
+    const newPasswordValue = newPassword.value.trim();
+    const confirmPasswordValue = confirmPassword.value.trim();
+
+    if(firstName === ''){ 
+        setError (firstName, 'First Name is required');
+    }else {
+        setSuccess(firstName);
+    }
+    if(lastName === ''){ 
+        setError (lastName, 'First Name is required');
+    }else {
+        setSuccess(lastName);
+    }
+    if(email === ''){ 
+        setError (email, 'First Name is required');
+    }else if (!isValidEmail(emailValue)) {
+        setError (email, 'Please provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(newPasswordValue === '') {
+        setError(password, 'Password is required');
+    } else if (newPasswordValue.length < 8) {
+        setError(newPassword, 'Password must be atleast 8 characters.')
+    } else {
+        setSuccess(newPassword);
+    }
+
+    if(confirmPasswordValue === '') {
+        setError(confirmPassword, 'Please confirm password');
+    } else if(confirmPasswordValue !== newPasswordValue) {
+        setError(confirmPassword, "Password doesn't match");
+    } else{
+        setSuccess(confirmPassword);
+    }
+};
